eb08f3f55506ee542e270b7e0bbc95b64bb03fb0
victorhugo_araujo <victorhugo_araujo@hotmail.com>
2019-12-23 07:34:19 -0300

    Layout

---
+++ src/components/profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+import './styles.css';
+
+const profile = () => {
+  return (
+    <div className='profile'>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  )
+}
+
+export default profile;
